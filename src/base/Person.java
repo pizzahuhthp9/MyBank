76ebd2cf02dc7a4db02aced59fe6ecdd54e45043
karn <karn1213@gmail.com>
2020-04-09 23:07:06 +0700

    getter / setter /toString

--- src/base/Person.java
+++ src/base/Person.java
@@ -10,5 +10,66 @@ package base;
  * @author 62130500127
  */
 public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String telephone;
+    private String email;
+    private String address;
+
+    public Person(String firstName, String lastName, String telephone, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephone = telephone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Person{" + "firstName=" + firstName + ", lastName=" + lastName + ", telephone=" + telephone + ", email=" + email + ", address=" + address + '}';
+    }
+
     
 }
