144d7bd720359a6d7dc09361793ada31f5650c71
62130500127 <62130500127@DESKTOP-1CCPJ6T>
2020-04-14 22:37:13 +0700

    add counting attributes rewrite Constructor add search methods add isFull methods add expandSize methods write createATM

--- src/bank/SubBank.java
+++ src/bank/SubBank.java
@@ -12,26 +12,71 @@ import machine.Machine;
  * @author 62130500127
  */
 public class SubBank {
-     private int vault;
+   private int vault;
    private String address;
-   private Machine[] ATMs;
+   private Machine[] machines;
+   private int machineCount;
    private BankAccount[] bankAccounts;
+   private int accountCount;
    private CounterService counterService;
    
-   public SubBank
-        (int vault,String address,CounterService counterService){
-       
-   }
-   public boolean addMoneyToATM(String Id,int money){
-       return true;
-   }
-   
-   public void createATM(){
-       
+   public SubBank(int vault,String address,CounterService counterService){
+       this.vault = vault;
+       this.address = address;
+       this.counterService = counterService;
+       machines = new Machine[1];
+       bankAccounts = new BankAccount[10];
    }
    
-   public void transferbankAccountsMoney
-        (int Money,String id,String address){
+   public int searchMachine(String id){
+       for (int i = 0; i < machines.length-1; i++) {
+           if (machines[i].getMachineId().equals(id)) {
+               return i;
+           }
+       }
+       return -1;
+   }
+   
+   public boolean isBankAccountsFull(){
+       if (bankAccounts.length == accountCount) {
+           return true;
+       }
+       return false;
+   }
+   
+   public boolean isMachineFull(){
+       if (machines.length == machineCount) {
+           return true;
+       }
+       return false;
+   }
+   
+   public boolean addMoneyToATM(String id,int money){
+       int index = searchMachine(id);
+       if (index >= 0) {
+           machines[index].receiveMoney(money);     //กานจะเขียน
+           return true;
+       }
+       return false;
+   }
+   
+   public void expandBankAccountsSize(){
+       BankAccount[] temp = new BankAccount[bankAccounts.length + 10];
+       System.arraycopy(bankAccounts, 0, temp, 0, bankAccounts.length);
+       bankAccounts = temp;
+   }
+   
+   public void createATM(String id){
+       int index = searchMachine(id);
+       if (index == -1){
+           if (isMachineFull()) {
+               expandBankAccountsSize();
+           }
+           
+       }
+   }
+   
+   public void transferbankAccountsMoney(int Money,String id,String address){
        
    }
    public void increaseMoney(int money,String id){
@@ -55,7 +100,7 @@ public class SubBank {
 
     @Override
     public String toString() {
-        return "SubBank{" + "vault=" + vault + ", address=" + address + ", ATMs=" + ATMs + ", bankAccounts=" + bankAccounts + ", counterService=" + counterService + '}';
+        return "SubBank{" + "vault=" + vault + ", address=" + address + ", ATMs=" + machines + ", bankAccounts=" + bankAccounts + ", counterService=" + counterService + '}';
     }
    
 }
